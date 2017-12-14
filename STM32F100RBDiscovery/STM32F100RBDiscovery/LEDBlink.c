e2de5a538483ec62fe9f504698f399f3503bcc2d
pawelwroc2000 <pawelmatecki@op.pl>
2017-12-14 18:46:48 +0100

    ok

--- STM32F100RBDiscovery/STM32F100RBDiscovery/LEDBlink.c
+++ STM32F100RBDiscovery/STM32F100RBDiscovery/LEDBlink.c
@@ -6,12 +6,27 @@ extern "C"
 
 int main(void)
 {
-	RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;	
-
-	GPIOC->BSRR = (1 << 8);
+	RCC->APB2ENR |= RCC_APB2ENR_IOPCEN | RCC_APB2ENR_IOPAEN;	
+		
+	GPIOC->CRH =  GPIO_CRH_MODE8_0 | GPIO_CRH_MODE9_0;
+	GPIOA->CRL |=  GPIO_CRL_CNF0_0;
 	
+	volatile unsigned long int i;
+	
+
 	for (;;)
 	{
+		if (GPIOA->IDR &(1<<0))
+		{
+			GPIOC->BSRR = 0xFFFF;			
+		}
+		else
+		{
+			GPIOC->BRR = 0xFFFF;			
+		};
 
+		//for (i = 0; i < 40000; ++i) ;
+
+		//for (i = 0; i < 40000; ++i) ;
 	}
 }
