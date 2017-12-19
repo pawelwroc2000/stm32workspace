88c4af15d0bf17a10e53a59b4d8a81fbabc2062d
pawelwroc2000 <pawelmatecki@op.pl>
2017-12-19 14:30:28 +0100

    ok

--- STM32F100VLDiscoveryCmsisTrainging/STM32F100VLDiscoveryCmsisTrainging/LEDBlink.c
+++ STM32F100VLDiscoveryCmsisTrainging/STM32F100VLDiscoveryCmsisTrainging/LEDBlink.c
@@ -10,20 +10,36 @@ void SysTick_Handler(void)
 	HAL_SYSTICK_IRQHandler();
 }
 
+
+void EXTI0_IRQHandler(void)
+{
+	HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_8);
+	EXTI->PR |= EXTI_PR_PIF0;
+	
+}
+
 int main(void)
 {
 	HAL_Init();
-
+	
 	__GPIOC_CLK_ENABLE();
+	
 	GPIO_InitTypeDef GPIO_InitStructure;
-
 	GPIO_InitStructure.Pin = GPIO_PIN_8;
-
 	GPIO_InitStructure.Mode = GPIO_MODE_OUTPUT_PP;
 	GPIO_InitStructure.Speed = GPIO_SPEED_HIGH;
 	GPIO_InitStructure.Pull = GPIO_NOPULL;
 	HAL_GPIO_Init(GPIOC, &GPIO_InitStructure);
 
+	RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
+	GPIOA->CRL |=  GPIO_CRL_CNF0_1;
+	
+	NVIC_EnableIRQ(EXTI0_IRQn);
+		
+//	RCC->APB2ENR |= RCC_APB2ENR_
+//	
+//	EXTI->RTSR |= 5;
+	
 	for (;;)
 	{
 		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, GPIO_PIN_SET);
